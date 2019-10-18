0f0684390e5bcb8614a3a7256bf5cd067427e787
olegastor <aleh_by@mail.ru>
2019-10-18 19:31:05 +0300

    много че уже добавил

--- app/src/main/java/by/znaj/rogachev2/historyapptest/VideoActivity.java
+++ app/src/main/java/by/znaj/rogachev2/historyapptest/VideoActivity.java
@@ -2,6 +2,16 @@ package by.znaj.rogachev2.historyapptest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import com.google.android.youtube.player.YouTubeBaseActivity;
@@ -9,7 +19,15 @@ import com.google.android.youtube.player.YouTubeInitializationResult;
 import com.google.android.youtube.player.YouTubePlayer;
 import com.google.android.youtube.player.YouTubePlayerView;
 
-public class VideoActivity extends YouTubeBaseActivity implements YouTubePlayer.OnInitializedListener {
+public class VideoActivity extends /*AppCompatActivity*/ YouTubeBaseActivity implements YouTubePlayer.OnInitializedListener {
+
+    private WebView mWebView;
+    private LinearLayout mContentView;
+    private FrameLayout mCustomViewContainer;
+    private WebChromeClient.CustomViewCallback mCustomViewCallback;
+    FrameLayout.LayoutParams COVER_SCREEN_GRAVITY_CENTER = new FrameLayout.LayoutParams(
+            ViewGroup.LayoutParams.WRAP_CONTENT,
+            ViewGroup.LayoutParams.WRAP_CONTENT, Gravity.CENTER);
 
     public static final String API_KEY = "AIzaSyA_SsxErQ96lpa6SM61llFb0ITHWhdyBbY";
 
@@ -32,6 +50,8 @@ public class VideoActivity extends YouTubeBaseActivity implements YouTubePlayer.
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video);
 
+        //getSupportActionBar().hide();
+
         YouTubePlayerView youTubePlayerView = (YouTubePlayerView) findViewById(R.id.youtube_player);
         youTubePlayerView.initialize(API_KEY, this);
 
@@ -46,11 +66,73 @@ public class VideoActivity extends YouTubeBaseActivity implements YouTubePlayer.
         }
         id = Integer.valueOf(Long.toString(taskId));
 
+        /*mContentView = (LinearLayout) findViewById(R.id.linearlayout);
+        mWebView = (WebView) findViewById(R.id.webView);
+        mCustomViewContainer = (FrameLayout) findViewById(R.id.fullscreen_custom_content);
+
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setPluginState(WebSettings.PluginState.ON);
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setUseWideViewPort(true);
+        webSettings.setLoadWithOverviewMode(true);
+
+        mWebView.loadUrl("https://www.youtube.com/watch?v=" + videoId[id]);
+        mWebView.setWebViewClient(new HelloWebViewClient());*/
+
+    }
+
+    /*private class HelloWebViewClient extends WebViewClient  {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView webview, String url)
+        {
+            webview.setWebChromeClient(new WebChromeClient() {
+
+                private View mCustomView;
+
+                @Override
+                public void onShowCustomView(View view, WebChromeClient.CustomViewCallback callback)
+                {
+                    // if a view already exists then immediately terminate the new one
+                    if (mCustomView != null)
+                    {
+                        callback.onCustomViewHidden();
+                        return;
+                    }
+
+                    // Add the custom view to its container.
+                    mCustomViewContainer.addView(view, COVER_SCREEN_GRAVITY_CENTER);
+                    mCustomView = view;
+                    mCustomViewCallback = callback;
+
+                    // hide main browser view
+                    mContentView.setVisibility(View.GONE);
+
+                    // Finally show the custom view container.
+                    mCustomViewContainer.setVisibility(View.VISIBLE);
+                    mCustomViewContainer.bringToFront();
+                }
+
+            });
+
+            webview.loadUrl(url);
+
+            return true;
+        }
     }
 
     @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event)
+    {
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && mWebView.canGoBack())
+        {
+            mWebView.goBack();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+
+    }*/
+    @Override
     public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
-        /** Start buffering **/
         if (!b) {
             youTubePlayer.cueVideo(videoId[id]);
         }
