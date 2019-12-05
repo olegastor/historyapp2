646efcf3599d0fbf858b028591576893711bfa8e
Oleg <aleh_by@mail.ru>
2019-12-05 12:47:19 +0300

    make word

--- app/src/main/java/by/znaj/rogachev2/historyapptest/IntActivity.java
+++ app/src/main/java/by/znaj/rogachev2/historyapptest/IntActivity.java
@@ -1,6 +1,10 @@
 package by.znaj.rogachev2.historyapptest;
 
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,6 +13,13 @@ public class IntActivity extends AppCompatActivity implements View.OnClickListen
 
 
     long taskId = 0;
+
+    private final int IDD_LIST = 1;
+
+    AlertDialog.Builder builder;
+
+    Context context;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +35,8 @@ public class IntActivity extends AppCompatActivity implements View.OnClickListen
         //findViewById(R.id.imgButton).setOnClickListener(this);
         findViewById(R.id.fillButton).setOnClickListener(this);
         findViewById(R.id.fillwordButton).setOnClickListener(this);
+
+        context = this;
 
     }
 
@@ -48,11 +61,12 @@ public class IntActivity extends AppCompatActivity implements View.OnClickListen
                 break;
             }*/
             case R.id.fillButton: {
-                Intent intent = new Intent(this, Fill2Activity.class);
+                showDialog(IDD_LIST);
+                /*Intent intent = new Intent(this, MakeWordActivity.class);
                 intent.putExtra("id", taskId);
                 intent.putExtra("trenType", 9);
                 startActivity(intent);
-                overridePendingTransition(R.anim.right_in, R.anim.left_out);
+                overridePendingTransition(R.anim.right_in, R.anim.left_out);*/
                 break;
             }
             case R.id.fillwordButton: {
@@ -76,5 +90,28 @@ public class IntActivity extends AppCompatActivity implements View.OnClickListen
     public void onBackPressed() {
         super.onBackPressed();
         goHome();
+    }
+
+    @Override
+    protected Dialog onCreateDialog(int id) {
+
+        final String[] modes ={"10 слов", "На время", "Без ограничений"};
+
+        builder = new AlertDialog.Builder(this);
+        builder.setTitle("Заполни слово \n Выберите режим:"); // заголовок для диалога
+
+        builder.setItems(modes, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int item) {
+                //mode = item;
+                Intent intent = new Intent(context, MakeWordActivity.class);
+                intent.putExtra("id", taskId);
+                intent.putExtra("mode", item);
+                startActivity(intent);
+                overridePendingTransition(R.anim.right_in, R.anim.left_out);
+            }
+        });
+        builder.setCancelable(false);
+        return builder.create();
     }
 }
